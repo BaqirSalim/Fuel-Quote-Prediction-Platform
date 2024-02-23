a13e9b5f4d2abea8d8548b859aa609ca15efca89
Akashew <akashnelson00@gmail.com>
2024-02-22 20:04:21 -0600

    transfer home  changes to login.jsx
    title

--- cosc_4353_project/src/routes/login.jsx
+++ cosc_4353_project/src/routes/login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "../styles/root.css"
 
 export default function Login(){
     const [username, setUsername] = useState('')
@@ -20,26 +21,31 @@ export default function Login(){
     }
     
     return (
-        <div>
-            <h2>Client Login</h2>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="log-username">Username:</label>
-                <input
-                type="text"
-                id="log-username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                /><br/>
-                <label htmlFor="log-password">Password:</label>
-                <input
-                type="password"
-                id="log-password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                /><br/>
-                <button>Login Here!</button>
-            </form>
-            <p>{error}</p>
+        <div className="container">
+            <div className="box">
+                <h2>Welcome</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="textbox">
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                    </div>
+                    <div className="textbox">
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <input type="submit" className="loginButton" value="Login" />
+                </form>
+                <p className="registerText">Don't have an account? <a href="/register">Register here</a></p>
+                <p>{error}</p>
+            </div>
         </div>
     );
 }
