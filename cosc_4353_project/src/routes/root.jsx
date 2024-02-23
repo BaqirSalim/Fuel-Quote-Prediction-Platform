392670de28803d1b81b64702ffbc2b01e73bb99c
Akashew <akashnelson00@gmail.com>
2024-02-22 18:57:14 -0600

    CSS for Login page
    styling for home

--- cosc_4353_project/src/routes/root.jsx
+++ cosc_4353_project/src/routes/root.jsx
@@ -1,9 +1,23 @@
+import React from 'react';
+import '../styles/root.css'; // Import the CSS file from the 'styles' folder
+
 export default function Root() {
     return (
         <div className="container">
-        <h1>Welcome to the Home Page</h1>
-        <a href="/login"><button className="loginButton">Login</button></a>
-        <a href="/register"><button className="registerButton">Register</button></a>
-    </div>
+            <div className="box">
+            
+                <form>
+                    <h2>Welcome!</h2>
+                    <div className="textbox">
+                        <input type="text" placeholder="Username" />
+                    </div>
+                    <div className="textbox">
+                        <input type="password" placeholder="Password" />
+                    </div>
+                    <input type="submit" className="loginButton" value="Login" />
+                </form>
+                <p className="registerText">Don't have an account? <a href="/register">Register here</a></p>
+            </div>
+        </div>
     );
-  }
+}
