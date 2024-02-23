419d176f3050cddcb292c77101ae649c6b2e0dd2
BaqirSalim <101154594+BaqirSalim@users.noreply.github.com>
2024-02-22 18:40:58 -0600

    registration submit handled

--- cosc_4353_project/src/routes/register.jsx
+++ cosc_4353_project/src/routes/register.jsx
@@ -4,15 +4,29 @@ import { useNavigate } from "react-router-dom";
 export default function Register(){
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
     const navigate = useNavigate()
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(username == '' || password == '')
+        {
+            setError("Username or Password empty")
+        }
+        else
+        {
+            navigate('/login')
+        }
+    }
+
     return (
         <div>
             <h2>Client Registration</h2>
-            <form action="/login" method="POST">
+            <form onSubmit={handleSubmit}>
                 <label htmlFor="reg-username">Username:</label>
                 <input
                 type="text"
-                id="re-username"
+                id="reg-username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
                 /><br/>
@@ -23,10 +37,9 @@ export default function Register(){
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 /><br/>
-                <button onClick={()=>
-                    navigate('/login')
-                }>Register Here!</button>
+                <button>Register Here!</button>
             </form>
+            <p>{error}</p>
         </div>
     );
 }
