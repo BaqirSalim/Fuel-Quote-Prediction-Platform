561d3972babecb61741b3fec8f021c1cae438518
matthewyohannes <matthewyohannes77@gmail.com>
2024-04-09 14:52:00 -0500

    connecting backend to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
+import mongoose from "mongoose"
+import cors from "cors";
+
 import clientRoutes from "./routes/client-profile.routes.js";
 import loginRoutes from "./routes/login.routes.js";
 import fuelRoutes from "./routes/fuel-quote.routes.js"
 
-import cors from "cors";
+
 const app = express();
 const port = 3000;
 
@@ -17,6 +20,16 @@ app.use("/client", clientRoutes);
 app.use("/user", loginRoutes);
 app.use("/fuelquote", fuelRoutes);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// MAKE SURE WE ADD ENV VARIABLES FOR THE USER AND PASSWORD HERE
+mongoose.connect(
+  "mongodb+srv://baamlogin4353:OHMPiYD6nkBdVL1x@cosc-4353-cluster.pdlclpo.mongodb.net/cosc-353-db?retryWrites=true&w=majority&appName=cosc-4353-cluster"
+)
+.then(() => {
+  console.log("Connected to database!");
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+})
+.catch(() => {
+  console.log("Connection failed!");
 });
