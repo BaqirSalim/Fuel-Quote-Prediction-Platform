ee9224c5ec9f2fd802c3cc88dcf7e14612290249
matthewyohannes <matthewyohannes77@gmail.com>
2024-04-09 15:46:38 -0500

    "added dotenv and .env file"

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose"
+import dotenv from "dotenv";
 import cors from "cors";
 
 import clientRoutes from "./routes/client-profile.routes.js";
@@ -9,6 +10,8 @@ import fuelRoutes from "./routes/fuel-quote.routes.js"
 
 const app = express();
 const port = 3000;
+
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -20,10 +23,9 @@ app.use("/client", clientRoutes);
 app.use("/user", loginRoutes);
 app.use("/fuelquote", fuelRoutes);
 
-// MAKE SURE WE ADD ENV VARIABLES FOR THE USER AND PASSWORD HERE
-mongoose.connect(
-  "mongodb+srv://baamlogin4353:OHMPiYD6nkBdVL1x@cosc-4353-cluster.pdlclpo.mongodb.net/cosc-353-db?retryWrites=true&w=majority&appName=cosc-4353-cluster"
-)
+
+
+mongoose.connect(process.env.MONGO_DB_URI)
 .then(() => {
   console.log("Connected to database!");
   app.listen(port, () => {
