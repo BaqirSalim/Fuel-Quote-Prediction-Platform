aac0b06b20119763166d643b3b4229be911766b3
matthewyohannes <matthewyohannes77@gmail.com>
2024-04-09 20:27:10 -0500

    added client profile model

--- backend/controllers/client-profile.controller.js
+++ backend/controllers/client-profile.controller.js
@@ -1,47 +1,19 @@
-import users from '../data/users.js'; 
+import Profile from '../models/client-profile.model.js'
 
 class ClientController {
     static async clientProfile(req, res) {
         try {
-            const { username, fullName, address1, address2, city, state, zipcode, orders } = req.body; // destructures data from frontend
+            const { username, fullName, address1, address2, city, state, zipcode, orders } = req.body; // destructures profile data from frontend
             
-            // finds the index of the current user in the users array
-            const userIndex = users.findIndex(user => user.username === username);
     
             if (!username || !fullName || !address1 || !city || !state || !zipcode) {
                 return res.status(400).json({ error: "One or more required fields is missing" });
             }
             
-            res.status(200).json({
-                username,
-                fullName,
-                address1,
-                address2,
-                city,
-                state,
-                zipcode,
-                orders
-            })
-            
-            
+            const profile = await Profile.create(req.body);
 
-            if (userIndex !== -1) {
-                // updates the user's data with the new profile information
-                users[userIndex] = {
-                    ...users[userIndex],
-                    username,
-                    fullName,
-                    address1,
-                    address2,
-                    city,
-                    state,
-                    zipcode,
-                    orders
-            };
-                res.status(200).json({ success: true, message: "Profile updated successfully" });
-            } else {
-                res.status(404).json({ error: "User not found" });
-            }
+            res.status(200).json({profile})
+            
     
         } catch (error) {
             console.log("Error in client profile controller", error.message)
--- backend/models/client-profile.model.js
+++ backend/models/client-profile.model.js
@@ -8,31 +8,42 @@ const clientProfileSchema = new mongoose.Schema({
     },
     fullName: {
         type: String,
-        required: true
+        required: true,
+        maxlength: 50
     },
     address1: {
         type: String,
         required: true,
-        minlength: 10
+        minlength: 10,
+        maxlength: 100
     },
     address2: {
         type: String,
         required: false,
-        minlength: 10
+        minlength: 10,
+        maxlength: 100
     },
     city: {
         type: String,
-        required: true
+        required: true,
+        maxlength: 100
     },
     state: {
         type: String,
-        required: true
+        required: true,
+        minlength: 2,
+        maxlength: 2
     },
     zipcode: {
         type: String,
-        required: true
+        required: true,
+        minlength: 5,
+        maxlength: 9
+    },
+    orders: {
+        type: [String], // Defines orders as an array of strings
+        default: [],    // Default value is an empty array
     }
-
 }, { timestamps: true });
 
 const ClientProfile = mongoose.model("ClientProfile", clientProfileSchema); // creates a client profile model to allow interaction with all of the client profiles in DB
