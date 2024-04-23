7ee56ebcfae29bfffe4615a0e1e1a0c7daf2b231
liltaqvi <alihussaintaqvi14@yahoo.com>
2024-04-23 16:28:30 -0500

    Comments added for Pricing module requirements

--- backend/controllers/pricing.js
+++ backend/controllers/pricing.js
@@ -9,11 +9,21 @@ class Pricing{
 }
 
 /*
-const pricing = require('./Pricing');
+Where,
 
-const gasPrice = 10;
-const gallonQuanity = 5;
+Current price per gallon = $1.50 (this is the price what distributor gets from refinery and it varies based upon crude price. But we are keeping it constant for simplicity)
+Margin =  Current Price * (Location Factor - Rate History Factor + Gallons Requested Factor + Company Profit Factor)
 
-const pricing = new Pricing(gasPrice);
-const totalPrice = pricing.calculateTotal(gallonQuantity);
+Consider these factors:
+Location Factor = 2% for Texas, 4% for out of state.
+Rate History Factor = 1% if client requested fuel before, 0% if no history (you can query fuel quote table to check if there are any rows for the client)
+Gallons Requested Factor = 2% if more than 1000 Gallons, 3% if less
+Company Profit Factor = 10% always
+
+Example:
+1500 gallons requested, in state, does have history (i.e. quote history data exist in DB for this client)
+
+Margin => (.02 - .01 + .02 + .1) * 1.50 = .195
+Suggested Price/gallon => 1.50 + .195 = $1.695
+Total Amount Due => 1500 * 1.695 = $2542.50 
 */
