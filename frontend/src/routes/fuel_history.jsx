3018a8fdc264275f99aa875cc7a9d867ab92dedc
matthewyohannes <matthewyohannes77@gmail.com>
2024-04-26 22:18:39 -0500

    akash made a silly mistake gotta save him

--- frontend/src/routes/fuel_history.jsx
+++ frontend/src/routes/fuel_history.jsx
@@ -1,41 +1,48 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "../context/authContext";
 import getFuelHistory from "../hooks/getFuelHistory";
+import "../styles/fuelhistory.css";
 
 export default function FuelHistory() {
-    const { getUser } = useAuth();
-    const [fuelQuotes, setFuelQuotes] = useState([]);
-    const user = getUser();
+  const { getUser } = useAuth();
+  const [fuelQuotes, setFuelQuotes] = useState([]);
+  const user = getUser();
 
-    const { fuelHistory } = getFuelHistory();
-    const fetchFuelQuotes = async () => {
-        try {
-            const data = await fuelHistory(user.username);
-            setFuelQuotes(data.fuelQuoteHistory);
-            console.log(data);
-        } catch (error) {
-            console.error("Error fetching fuel history:", error);
-        }
-    };
-    useEffect(() => {
-        fetchFuelQuotes();
-    }, [user.username]);
+  const { fuelHistory } = getFuelHistory();
+  const fetchFuelQuotes = async () => {
+    try {
+      const data = await fuelHistory(user.username);
+      setFuelQuotes(data.fuelQuoteHistory);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching fuel history:", error);
+    }
+  };
+  useEffect(() => {
+    fetchFuelQuotes();
+  }, [user.username]);
 
-    return (
-        <div>
-            <h1>Fuel Quote History</h1>
-            <ul>
-                {fuelQuotes.map((quote) => (
-                    <li key={quote._id}>
-                        <p>Gallons Requested: {quote.gallonsRequested}</p>
-                        <p>Delivery Address: {quote.deliveryAddress}</p>
-                        <p>Delivery Date: {quote.deliveryDate}</p>
-                        <p>Suggested Price: {quote.suggestedPrice}</p>
-                        <p>Total Amount Due: {quote.totalAmountDue}</p>
-                        <p>Created At: {quote.createdAt}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Fuel Quote History</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Gallons Requested</th>
+            <th>Total Amount Due</th>
+          </tr>
+        </thead>
+        <tbody>
+          {fuelQuotes.map((quote) => (
+            <tr key={quote._id}>
+              <td className="table-cell">{quote.gallonsRequested}</td>
+              <td className="table-cell">{quote.totalAmountDue}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+  
+  
 }
